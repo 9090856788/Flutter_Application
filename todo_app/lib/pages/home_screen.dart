50519283154216038e3ae4ceeeb21e4838c6d929
Kanhu Charan Sahoo <kanhucharansahoo595@gmail.com>
2026-01-04 18:17:27 +0530

    created todo tiles

--- todo_app/lib/pages/home_screen.dart
+++ todo_app/lib/pages/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../utils/todo_tile.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,6 +9,13 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // create a list to hold our to-do items
+  List toDoList = [
+    ['Make Tutorial', false],
+    ['Do Exercise', false],
+    ['Build App', false],
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,6 +25,21 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
         backgroundColor: Colors.yellow[400],
         elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return TodoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) {
+              // Handle checkbox state change
+              setState(() {
+                toDoList[index][1] = value;
+              });
+            },
+          );
+        },
       ),
     );
   }
