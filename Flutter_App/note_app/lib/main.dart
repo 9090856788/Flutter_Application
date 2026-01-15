4e646a5ef09e41e8d405f89a8c096c72b4f64512
kanhucharansahoo <kanhucharansahoo595@gmail.com>
2026-01-16 00:23:13 +0530

    create a flutter app & configure boiler plate of note app

---
+++ Flutter_App/note_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import './pages/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Neumorphism_Container_Widget",
+      theme: ThemeData(primarySwatch: Colors.grey),
+      home: HomeScreen(),
+    );
+  }
+}
