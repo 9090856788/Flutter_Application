19b74d5b291c9956039ebea07fbf75f93e618e36
kanhucharansahoo <kanhucharansahoo595@gmail.com>
2026-01-15 23:29:54 +0530

    change the app name

--- Flutter_UI/neumorphism_container_widget/lib/main.dart
+++ Flutter_UI/neumorphism_container_widget/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:neumorphism_container_wiget/pages/home_screen.dart';
+import './pages/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: "Neumorphism_Container_Wiget",
+      title: "Neumorphism_Container_Widget",
       theme: ThemeData(primarySwatch: Colors.grey),
       home: HomeScreen(),
     );
