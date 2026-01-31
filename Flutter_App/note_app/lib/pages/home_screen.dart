fc8c5161ae38e3da9dde94f4120e2deddb51016f
kanhucharansahoo <kanhucharansahoo595@gmail.com>
2026-01-31 23:33:36 +0530

    create a flutter app & config the boiler plate code

---
+++ Flutter_App/note_app/lib/pages/home_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple[200],
+        elevation: 0,
+        title: const Text("N O T E A P P"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
