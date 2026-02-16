ef51fbe1cacec40c030375dcc30001e931864ac0
kanhucharansahoo <kanhucharansahoo595@gmail.com>
2026-02-16 23:37:21 +0530

    created constants files for reusability

---
+++ Flutter_App/e_commerce_app_kit/lib/constants/app_assets.dart
@@ -0,0 +1,13 @@
+/// Asset path constants. Update paths here if you move/rename files in `pubspec.yaml`.
+class AppAssets {
+  AppAssets._();
+
+  // Existing asset in this project
+  static const String avatar = 'images/person.jpg';
+
+  // Example placeholders (add these files under `images/` if you plan to use them)
+  static const String logo = 'images/logo.png';
+  static const String background = 'images/background.png';
+  static const String icEmail = 'images/icons/ic_email.png';
+  static const String icPassword = 'images/icons/ic_password.png';
+}
---
+++ Flutter_App/e_commerce_app_kit/lib/constants/app_text_styles.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+import 'app_sizes.dart';
+
+/// Reusable `TextStyle` definitions.
+class AppTextStyles {
+  AppTextStyles._();
+
+  static const TextStyle heading = TextStyle(
+    color: AppColors.surface,
+    fontSize: AppSizes.fontXLarge,
+    fontWeight: FontWeight.bold,
+  );
+
+  static const TextStyle title = TextStyle(
+    color: AppColors.surface,
+    fontSize: AppSizes.fontLarge,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle body = TextStyle(
+    color: AppColors.textSecondary,
+    fontSize: AppSizes.fontRegular,
+  );
+
+  static const TextStyle button = TextStyle(
+    color: Colors.black,
+    fontSize: AppSizes.fontRegular,
+    fontWeight: FontWeight.w600,
+  );
+}
