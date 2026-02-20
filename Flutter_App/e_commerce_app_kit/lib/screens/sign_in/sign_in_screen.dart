8643273f00a56b6fd80113c3d8f1329a69f3e6f1
kanhucharansahoo <kanhucharansahoo595@gmail.com>
2026-02-21 01:29:06 +0530

    implement the login page

--- Flutter_App/e_commerce_app_kit/lib/screens/sign_in/sign_in_screen.dart
+++ Flutter_App/e_commerce_app_kit/lib/screens/sign_in/sign_in_screen.dart
@@ -1,7 +1,9 @@
+import 'package:e_commerce_app_kit/components/default_button.dart';
 import 'package:e_commerce_app_kit/constants/app_colors.dart';
 import 'package:e_commerce_app_kit/constants/size_config.dart';
 import 'package:e_commerce_app_kit/screens/sign_in/components/sign_in_form.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
@@ -59,8 +61,129 @@ class _SignInScreenState extends State<SignInScreen> {
                 child: SignInForm(),
               ),
             ),
-            Expanded(flex: 2, child: Container(color: Colors.grey)),
-            Expanded(flex: 2, child: Container(color: Colors.blue)),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(
+                      right: 20.0,
+                      top: 0,
+                      bottom: 25.0,
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Checkbox(
+                              value: false,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  value =
+                                      value!; // Update the checkbox value state
+                                  // Update the checkbox value state
+                                });
+                              },
+                            ),
+                            const Text("Remember me"),
+                          ],
+                        ),
+                        GestureDetector(
+                          onTap: () {},
+                          child: const Text(
+                            "Forgot password",
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  DefaultButton(text: "Sign in", onPressed: () {}),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                children: [
+                  Spacer(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColors.primaryLightColor,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: SvgPicture.asset(
+                            "assets/icons/google-icon.svg",
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColors.primaryLightColor,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: SvgPicture.asset(
+                            "assets/icons/facebook-2.svg",
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 10),
+                      Container(
+                        width: 50,
+                        height: 50,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: AppColors.primaryLightColor,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: SvgPicture.asset("assets/icons/twitter.svg"),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account?",
+                        style: TextStyle(
+                          fontSize: getProportionateScreenWidth(18),
+                        ),
+                      ),
+                      GestureDetector(
+                        onTap: () {},
+                        child: Text(
+                          " Sign Up",
+                          style: TextStyle(
+                            fontSize: getProportionateScreenWidth(18),
+                            color: AppColors.primaryColor,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Spacer(),
+                ],
+              ),
+            ),
           ],
         ),
       ),
