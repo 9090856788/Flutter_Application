a2c19fd4a273c739517a114a08e266e607fc70e0
kanhucharansahoo <kanhucharansahoo595@gmail.com>
2026-03-06 17:22:48 +0530

    created webview in flutter app

--- Flutter_Widget/webview_in_flutter_app/lib/screens/home_screen.dart
+++ Flutter_Widget/webview_in_flutter_app/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:webview_in_flutter_app/screens/web_view_url.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,23 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("WebView in Flutter"),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => WebViewUrl()),
+            );
+          },
+          child: Text("Open Github"),
+        ),
+      ),
+    );
   }
 }
